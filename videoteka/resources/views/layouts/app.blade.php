25bb2ccab7033b44ab92edd56336f3d40421c657
NikolaD01 <devicnikola01@gmail.com>
2023-06-30 19:15:56 +0200

    blade

--- videoteka/resources/views/layouts/app.blade.php
+++ videoteka/resources/views/layouts/app.blade.php
@@ -30,9 +30,14 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('lang', ['locale' => 'en']); }}">EN</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('lang', ['locale' => 'sr']); }}">SR</a>
+                        </li>
                     </ul>
-                        
+
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
